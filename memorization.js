eb193fb75ad6ed6c332b0fdaa2343a26a5b253ad
Kyle Rodgers <krodgers4@mail.csuchico.edu>
2016-12-05 19:53:53 -0800

    Basic website done with the functionality to make user have to enter their name before proceeding

---
+++ memorization.js
@@ -0,0 +1,42 @@
+var personName = "";
+function check(){
+  if (personName == "") {
+    event.preventDefault();
+    window.alert("Enter your name before proceeding!")
+  }
+  else if (isNaN(personName) == false) {
+    event.preventDefault();
+    window.alert("Your name cannot consist of numbers!")
+  }
+}
+function saveName(){
+  var input = document.getElementById("save").value;
+  console.log(input);
+  setCookie("name", input);
+  personName = input;
+}
+function getName(){
+  document.getElementById("name1").innerHTML = personName;
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
