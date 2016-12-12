a2d3de5be4f68129672db13b1518c08d539022aa
Kyle Rodgers <krodgers4@mail.csuchico.edu>
2016-12-11 19:05:50 -0800

    Working on showing results on sportsresults.html

--- memorization.js
+++ memorization.js
@@ -24,11 +24,31 @@ function getName(){
 function loadName(){
   var personName = getCookie("name");
 }
+function results(){
+  setCookie("numberCorrect", amountCorrect);
+  stAnswers = stAnswers.join;
+  setCookie("arrayCorrect", stAnswers);
+}
+function loadResults(){
+  var total = 0;
+  var userInput = getCookie("name");
+  document.getElementById("name1").innerHTML = "Hello" + " " + userInput;
+  var correct = getCookie("numberCorrect");
+  correct = correct / 25;
+  correct = correct * 100;
+  var wrongCorrect = getCookie("arrayCorrect");
+  wrongCorrect = wrongCorrect.split();
+  if (wrongCorrect.indexOf(cardinals) != -1) {
+    total = total + 1;
+  }
+  document.getElementById("resultText").innerHTML =
+}
 function submitInput1(){
 var answer1;
 answer1 = document.getElementById("i1").value;
 answer1 = answer1.toLowerCase(answer1);
 stAnswers.push(answer1);
+document.getElementById("i1").disabled = true;
 if (answer1 == "cardinals") {
   amountCorrect + 1;
   }
@@ -38,6 +58,7 @@ var answer2;
 answer2 = document.getElementById("i2").value;
 answer2 = answer2.toLowerCase(answer2);
 stAnswers.push(answer2);
+document.getElementById("i2").disabled = true;
 if (answer2 == "falcons") {
   amountCorrect + 1;
   }
@@ -47,6 +68,7 @@ var answer3;
 answer3 = document.getElementById("i3").value;
 answer3 = answer3.toLowerCase(answer3);
 stAnswers.push(answer3);
+document.getElementById("i3").disabled = true;
 if (answer3 == "ravens") {
   amountCorrect + 1;
   }
@@ -56,6 +78,7 @@ var answer4;
 answer4 = document.getElementById("i4").value;
 answer4 = answer4.toLowerCase(answer4);
 stAnswers.push(answer4);
+document.getElementById("i4").disabled = true;
 if (answer4 == "bills") {
   amountCorrect + 1;
   }
@@ -65,6 +88,7 @@ var answer5;
 answer5 = document.getElementById("i5").value;
 answer5 = answer5.toLowerCase(answer5);
 stAnswers.push(answer5);
+document.getElementById("i5").disabled = true;
 if (answer5 == "panthers") {
   amountCorrect + 1;
   }
@@ -74,6 +98,7 @@ var answer6;
 answer6 = document.getElementById("i6").value;
 answer6 = answer6.toLowerCase(answer6);
 stAnswers.push(answer6);
+document.getElementById("i6").disabled = true;
 if (answer6 == "bears") {
   amountCorrect + 1;
   }
@@ -83,6 +108,7 @@ var answer7;
 answer7 = document.getElementById("i7").value;
 answer7 = answer7.toLowerCase(answer7);
 stAnswers.push(answer7);
+document.getElementById("i7").disabled = true;
 if (answer7 == "bengals") {
   amountCorrect + 1;
   }
@@ -92,6 +118,7 @@ var answer8;
 answer8 = document.getElementById("i8").value;
 answer8 = answer8.toLowerCase(answer8);
 stAnswers.push(answer8);
+document.getElementById("i8").disabled = true;
 if (answer8 == "browns") {
   amountCorrect + 1;
   }
@@ -101,6 +128,7 @@ var answer9;
 answer9 = document.getElementById("i9").value;
 answer9 = answer9.toLowerCase(answer9);
 stAnswers.push(answer9);
+document.getElementById("i9").disabled = true;
 if (answer9 == "cowboys") {
   amountCorrect + 1;
   }
@@ -110,6 +138,7 @@ var answer10;
 answer10 = document.getElementById("i10").value;
 answer10 = answer10.toLowerCase(answer10);
 stAnswers.push(answer10);
+document.getElementById("i10").disabled = true;
 if (answer10 == "broncos") {
   amountCorrect + 1;
   }
@@ -119,6 +148,7 @@ var answer11;
 answer11 = document.getElementById("i11").value;
 answer11 = answer11.toLowerCase(answer11);
 stAnswers.push(answer11);
+document.getElementById("i11").disabled = true;
 if (answer11 == "lions") {
   amountCorrect + 1;
   }
@@ -128,6 +158,7 @@ var answer12;
 answer12 = document.getElementById("i12").value;
 answer12 = answer12.toLowerCase(answer12);
 stAnswers.push(answer12);
+document.getElementById("i12").disabled = true;
 if (answer12 == "packers") {
   amountCorrect + 1;
   }
@@ -137,6 +168,7 @@ var answer13;
 answer13 = document.getElementById("i13").value;
 answer13 = answer13.toLowerCase(answer13);
 stAnswers.push(answer13);
+document.getElementById("i13").disabled = true;
 if (answer13 == "texans") {
   amountCorrect + 1;
   }
@@ -146,6 +178,7 @@ var answer14;
 answer14 = document.getElementById("i14").value;
 answer14 = answer14.toLowerCase(answer14);
 stAnswers.push(answer14);
+document.getElementById("i14").disabled = true;
 if (answer14 == "colts") {
   amountCorrect + 1;
   }
@@ -155,6 +188,7 @@ var answer15;
 answer15 = document.getElementById("i15").value;
 answer15 = answer15.toLowerCase(answer15);
 stAnswers.push(answer15);
+document.getElementById("i15").disabled = true;
 if (answer15 == "jaguars") {
   amountCorrect + 1;
   }
@@ -164,6 +198,7 @@ var answer16;
 answer16 = document.getElementById("i16").value;
 answer16 = answer16.toLowerCase(answer16);
 stAnswers.push(answer16);
+document.getElementById("i16").disabled = true;
 if (answer16 == "chiefs") {
   amountCorrect + 1;
   }
@@ -173,6 +208,7 @@ var answer17;
 answer17 = document.getElementById("i17").value;
 answer17 = answer17.toLowerCase(answer17);
 stAnswers.push(answer17);
+document.getElementById("i17").disabled = true;
 if (answer17 == "rams") {
   amountCorrect + 1;
   }
@@ -182,6 +218,7 @@ var answer18;
 answer18 = document.getElementById("i18").value;
 answer18 = answer18.toLowerCase(answer18);
 stAnswers.push(answer18);
+document.getElementById("i18").disabled = true;
 if (answer18 == "dolphins") {
   amountCorrect + 1;
   }
@@ -191,6 +228,7 @@ var answer19;
 answer19 = document.getElementById("i19").value;
 answer19 = answer19.toLowerCase(answer19);
 stAnswers.push(answer19);
+document.getElementById("i19").disabled = true;
 if (answer19 == "vikings") {
   amountCorrect + 1;
   }
@@ -200,6 +238,7 @@ var answer20;
 answer20 = document.getElementById("i20").value;
 answer20 = answer20.toLowerCase(answer20);
 stAnswers.push(answer20);
+document.getElementById("i20").disabled = true;
 if (answer20 == "patriots") {
   amountCorrect + 1;
   }
@@ -209,6 +248,7 @@ var answer21;
 answer21 = document.getElementById("i21").value;
 answer21 = answer21.toLowerCase(answer21);
 stAnswers.push(answer21);
+document.getElementById("i21").disabled = true;
 if (answer21 == "saints") {
   amountCorrect + 1;
   }
@@ -218,6 +258,7 @@ var answer22;
 answer22 = document.getElementById("i22").value;
 answer22 = answer22.toLowerCase(answer22);
 stAnswers.push(answer22);
+document.getElementById("i22").disabled = true;
 if (answer22 == "giants") {
   amountCorrect + 1;
   }
@@ -227,6 +268,7 @@ var answer23;
 answer23 = document.getElementById("i23").value;
 answer23 = answer23.toLowerCase(answer23);
 stAnswers.push(answer23);
+document.getElementById("i23").disabled = true;
 if (answer23 == "eagles") {
   amountCorrect + 1;
   }
@@ -236,6 +278,7 @@ var answer24;
 answer24 = document.getElementById("i24").value;
 answer24 = answer24.toLowerCase(answer24);
 stAnswers.push(answer24);
+document.getElementById("i24").disabled = true;
 if (answer24 == "chargers") {
   amountCorrect + 1;
   }
@@ -245,6 +288,7 @@ var answer25;
 answer25 = document.getElementById("i25").value;
 answer25 = answer25.toLowerCase(answer25);
 stAnswers.push(answer25);
+document.getElementById("i25").disabled = true;
 if (answer25 == "buccaneers") {
   amountCorrect + 1;
   }
