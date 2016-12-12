eb20ff1ea35ddd6f256f659be9cdb44310f2e6f9
Kyle Rodgers <krodgers4@mail.csuchico.edu>
2016-12-12 00:51:55 -0800

    Results should work now

--- memorization2.js
+++ memorization2.js
@@ -7,7 +7,7 @@ function getName(){
 function results(){
   setCookie("numberCorrect", amountCorrect);
   vgAnswers = vgAnswers.toString();
-  setCookie("arrayCorrect", stAnswers);
+  setCookie("arrayCorrect", vgAnswers);
 }
 function loadResults(){
   var total = 0;
@@ -101,7 +101,7 @@ function submitInput1(){
 var answer1;
 answer1 = document.getElementById("i1").value;
 answer1 = answer1.toLowerCase(answer1);
-stAnswers.push(answer1);
+vgAnswers.push(answer1);
 document.getElementById("i1").disabled = true;
 if (answer1 == "aatrox") {
   amountCorrect = amountCorrect + 1;
@@ -111,7 +111,7 @@ function submitInput2(){
 var answer2;
 answer2 = document.getElementById("i2").value;
 answer2 = answer2.toLowerCase(answer2);
-stAnswers.push(answer2);
+vgAnswers.push(answer2);
 document.getElementById("i2").disabled = true;
 if (answer2 == "chogath") {
   amountCorrect = amountCorrect + 1;
@@ -121,7 +121,7 @@ function submitInput3(){
 var answer3;
 answer3 = document.getElementById("i3").value;
 answer3 = answer3.toLowerCase(answer3);
-stAnswers.push(answer3);
+vgAnswers.push(answer3);
 document.getElementById("i3").disabled = true;
 if (answer3 == "amumu") {
   amountCorrect = amountCorrect + 1;
@@ -131,7 +131,7 @@ function submitInput4(){
 var answer4;
 answer4 = document.getElementById("i4").value;
 answer4 = answer4.toLowerCase(answer4);
-stAnswers.push(answer4);
+vgAnswers.push(answer4);
 document.getElementById("i4").disabled = true;
 if (answer4 == "blitzcrank") {
   amountCorrect = amountCorrect + 1;
@@ -141,7 +141,7 @@ function submitInput5(){
 var answer5;
 answer5 = document.getElementById("i5").value;
 answer5 = answer5.toLowerCase(answer5);
-stAnswers.push(answer5);
+vgAnswers.push(answer5);
 document.getElementById("i5").disabled = true;
 if (answer5 == "fizz") {
   amountCorrect = amountCorrect + 1;
@@ -151,7 +151,7 @@ function submitInput6(){
 var answer6;
 answer6 = document.getElementById("i6").value;
 answer6 = answer6.toLowerCase(answer6);
-stAnswers.push(answer6);
+vgAnswers.push(answer6);
 document.getElementById("i6").disabled = true;
 if (answer6 == "ahri") {
   amountCorrect = amountCorrect + 1;
@@ -161,7 +161,7 @@ function submitInput7(){
 var answer7;
 answer7 = document.getElementById("i7").value;
 answer7 = answer7.toLowerCase(answer7);
-stAnswers.push(answer7);
+vgAnswers.push(answer7);
 document.getElementById("i7").disabled = true;
 if (answer7 == "alistar") {
   amountCorrect = amountCorrect + 1;
@@ -171,7 +171,7 @@ function submitInput8(){
 var answer8;
 answer8 = document.getElementById("i8").value;
 answer8 = answer8.toLowerCase(answer8);
-stAnswers.push(answer8);
+vgAnswers.push(answer8);
 document.getElementById("i8").disabled = true;
 if (answer8 == "galio") {
   amountCorrect = amountCorrect + 1;
@@ -181,7 +181,7 @@ function submitInput9(){
 var answer9;
 answer9 = document.getElementById("i9").value;
 answer9 = answer9.toLowerCase(answer9);
-stAnswers.push(answer9);
+vgAnswers.push(answer9);
 document.getElementById("i9").disabled = true;
 if (answer9 == "brand") {
   amountCorrect = amountCorrect + 1;
@@ -191,7 +191,7 @@ function submitInput10(){
 var answer10;
 answer10 = document.getElementById("i10").value;
 answer10 = answer10.toLowerCase(answer10);
-stAnswers.push(answer10);
+vgAnswers.push(answer10);
 document.getElementById("i10").disabled = true;
 if (answer10 == "fiddlesticks") {
   amountCorrect = amountCorrect + 1;
@@ -201,7 +201,7 @@ function submitInput11(){
 var answer11;
 answer11 = document.getElementById("i11").value;
 answer11 = answer11.toLowerCase(answer11);
-stAnswers.push(answer11);
+vgAnswers.push(answer11);
 document.getElementById("i11").disabled = true;
 if (answer11 == "hecarim") {
   amountCorrect = amountCorrect + 1;
@@ -211,7 +211,7 @@ function submitInput12(){
 var answer12;
 answer12 = document.getElementById("i12").value;
 answer12 = answer12.toLowerCase(answer12);
-stAnswers.push(answer12);
+vgAnswers.push(answer12);
 document.getElementById("i12").disabled = true;
 if (answer12 == "karthus") {
   amountCorrect = amountCorrect + 1;
@@ -221,7 +221,7 @@ function submitInput13(){
 var answer13;
 answer13 = document.getElementById("i13").value;
 answer13 = answer13.toLowerCase(answer13);
-stAnswers.push(answer13);
+vgAnswers.push(answer13);
 document.getElementById("i13").disabled = true;
 if (answer13 == "xerath") {
   amountCorrect = amountCorrect + 1;
@@ -231,7 +231,7 @@ function submitInput14(){
 var answer14;
 answer14 = document.getElementById("i14").value;
 answer14 = answer14.toLowerCase(answer14);
-stAnswers.push(answer14);
+vgAnswers.push(answer14);
 document.getElementById("i14").disabled = true;
 if (answer14 == "yasuo") {
   amountCorrect = amountCorrect + 1;
@@ -241,7 +241,7 @@ function submitInput15(){
 var answer15;
 answer15 = document.getElementById("i15").value;
 answer15 = answer15.toLowerCase(answer15);
-stAnswers.push(answer15);
+vgAnswers.push(answer15);
 document.getElementById("i15").disabled = true;
 if (answer15 == "ziggs") {
   amountCorrect = amountCorrect + 1;
@@ -251,7 +251,7 @@ function submitInput16(){
 var answer16;
 answer16 = document.getElementById("i16").value;
 answer16 = answer16.toLowerCase(answer16);
-stAnswers.push(answer16);
+vgAnswers.push(answer16);
 document.getElementById("i16").disabled = true;
 if (answer16 == "zed") {
   amountCorrect = amountCorrect + 1;
@@ -261,7 +261,7 @@ function submitInput17(){
 var answer17;
 answer17 = document.getElementById("i17").value;
 answer17 = answer17.toLowerCase(answer17);
-stAnswers.push(answer17);
+vgAnswers.push(answer17);
 document.getElementById("i17").disabled = true;
 if (answer17 == "warwick") {
   amountCorrect = amountCorrect + 1;
@@ -271,7 +271,7 @@ function submitInput18(){
 var answer18;
 answer18 = document.getElementById("i18").value;
 answer18 = answer18.toLowerCase(answer18);
-stAnswers.push(answer18);
+vgAnswers.push(answer18);
 document.getElementById("i18").disabled = true;
 if (answer18 == "teemo") {
   amountCorrect = amountCorrect + 1;
@@ -281,7 +281,7 @@ function submitInput19(){
 var answer19;
 answer19 = document.getElementById("i19").value;
 answer19 = answer19.toLowerCase(answer19);
-stAnswers.push(answer19);
+vgAnswers.push(answer19);
 document.getElementById("i19").disabled = true;
 if (answer19 == "thresh") {
   amountCorrect = amountCorrect + 1;
@@ -291,7 +291,7 @@ function submitInput20(){
 var answer20;
 answer20 = document.getElementById("i20").value;
 answer20 = answer20.toLowerCase(answer20);
-stAnswers.push(answer20);
+vgAnswers.push(answer20);
 document.getElementById("i20").disabled = true;
 if (answer20 == "singed") {
   amountCorrect = amountCorrect + 1;
@@ -301,7 +301,7 @@ function submitInput21(){
 var answer21;
 answer21 = document.getElementById("i21").value;
 answer21 = answer21.toLowerCase(answer21);
-stAnswers.push(answer21);
+vgAnswers.push(answer21);
 document.getElementById("i21").disabled = true;
 if (answer21 == "nocturne") {
   amountCorrect = amountCorrect + 1;
@@ -311,7 +311,7 @@ function submitInput22(){
 var answer22;
 answer22 = document.getElementById("i22").value;
 answer22 = answer22.toLowerCase(answer22);
-stAnswers.push(answer22);
+vgAnswers.push(answer22);
 document.getElementById("i22").disabled = true;
 if (answer22 == "malzahar") {
   amountCorrect = amountCorrect + 1;
@@ -321,7 +321,7 @@ function submitInput23(){
 var answer23;
 answer23 = document.getElementById("i23").value;
 answer23 = answer23.toLowerCase(answer23);
-stAnswers.push(answer23);
+vgAnswers.push(answer23);
 document.getElementById("i23").disabled = true;
 if (answer23 == "kennen") {
   amountCorrect = amountCorrect + 1;
@@ -331,7 +331,7 @@ function submitInput24(){
 var answer24;
 answer24 = document.getElementById("i24").value;
 answer24 = answer24.toLowerCase(answer24);
-stAnswers.push(answer24);
+vgAnswers.push(answer24);
 document.getElementById("i24").disabled = true;
 if (answer24 == "kindred") {
   amountCorrect = amountCorrect + 1;
@@ -341,7 +341,7 @@ function submitInput25(){
 var answer25;
 answer25 = document.getElementById("i25").value;
 answer25 = answer25.toLowerCase(answer25);
-stAnswers.push(answer25);
+vgAnswers.push(answer25);
 document.getElementById("i25").disabled = true;
 if (answer25 == "gangplank") {
   amountCorrect = amountCorrect + 1;
