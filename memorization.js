09222e0eae97b95ec02f5cae19cec0dbba39967c
Kyle Rodgers <krodgers4@mail.csuchico.edu>
2016-12-11 21:08:46 -0800

    More work towards geting results to display properly

--- memorization.js
+++ memorization.js
@@ -41,7 +41,10 @@ function loadResults(){
   if (wrongCorrect.indexOf(cardinals) != -1) {
     total = total + 1;
   }
-  document.getElementById("resultText").innerHTML = correct;
+  wrongCorrect = wrongCorrect / 25;
+  wrongCorrect = wrongCorrect * 100;
+  document.getElementById("resultText").innerHTML = correct + "%" + "correct." + "<br>"
+  + wrongCorrect + "%" + "correct names but wrong places."
 }
 function submitInput1(){
 var answer1;
